d20136570a4f243dfcf6019b80c69412f6936572
Victor-Lis <victorlisbronzo1@gmail.com>
2023-12-31 17:40:35 -0300

    v1.3

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ const nextYear = new Date().getFullYear() + 1
 const nextTime = new Date(`January 01 ${nextYear} 00:00:00`)
 
 function yearCalc(){
-    const thisYear = new Date()
-    const difference = nextTime - thisYear
-    const days = Math.floor((((difference / 1000) / 60) / 60) / 24)
-    const hours = Math.floor(((difference / 1000) / 60) / 60) % 24
-    const minutes = Math.floor((difference / 1000) / 60) % 60
-    const seconds = Math.floor(difference / 1000) % 60    
+
+    const actualYear = new Date()
+    const diff = nextTime - actualYear
+
+    const days = Math.floor((((diff / 1000) / 60) / 60) / 24)
+    const hours = Math.floor(((diff / 1000) / 60) / 60) % 24
+    const minutes = Math.floor((diff / 1000) / 60) % 60
+    const seconds = Math.floor(diff / 1000) % 60    
 
     printTimer((days < 10? '0'+days: days), (hours < 10? '0'+hours: hours), (minutes < 10? '0'+minutes: minutes), (seconds < 10? '0'+seconds: seconds))
+    
 }
 
 function printTimer(days, hours, minutes, seconds){
